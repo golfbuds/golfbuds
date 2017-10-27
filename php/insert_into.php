ca23f0bed3e0ceed3a91dd81de88a549ad65bc39
golfbuds <32232273+golfbuds@users.noreply.github.com>
2017-10-27 02:51:46 -0700

    Anthony Changes 10/27/2017
    error checking, updated connections to database

--- php/insert_into.php
+++ php/insert_into.php
@@ -1,22 +1,57 @@
 <?php
 include "db_connect.php";
 
-$first_name = $last_name = $user_name = $user_email = $user_pass = "";
+$first_name = $last_name = $user_name = $user_email = $user_pass = $user_pass_confirm = "";
+$fail = false;
 
-//if($_SERVER["REQUEST_METHOD"] == "POST"){
-$first_name = $_POST["firstname"];
-$last_name = $_POST["lastname"];
-$user_name = $_POST["uname"];
-$user_email = $_POST["em"];
-$user_pass = $_POST["passW"];
+//Check if input is from post method and check if null.
+if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["firstname"]) && isset($_POST["lastname"]) 
+		&& isset($_POST["uname"]) && isset($_POST["em"]) && isset($_POST["passW"]) && isset($_POST["passWConfirm"])) {
+	
+	//Get values and trim spaces, strip input of html and php tags, escape input 	
+	$first_name = mysqli_real_escape_string($conn , strip_tags(trim($_POST["firstname"])));
+	$last_name = mysqli_real_escape_string($conn, strip_tags(trim($_POST["lastname"])));
+	$user_name = mysqli_real_escape_string($conn ,strip_tags(trim($_POST["uname"])));
+	//Also sanitize illegal inputs for email
+	$user_email = mysqli_real_escape_string($conn, filter_var(strip_tags(trim($_POST["em"])), FILTER_SANITIZE_EMAIL));
+	$user_pass = mysqli_real_escape_string($conn, strip_tags(trim($_POST["passW"])));
+	$user_pass_confirm = mysqli_real_escape_string($conn, strip_tags(trim($_POST["passWConfirm"])));
+	
 
+} else {
+	$fail = true;
+}
 
+//check if empty string
+if(strcmp($first_name, "") === 0 || strcmp($last_name, "") === 0 || strcmp($user_name, "") === 0 
+			|| strcmp($user_email, "") === 0 || strcmp($user_pass, "") === 0  || strcmp($user_pass_confirm, "") === 0) { 
 
-$sql = "INSERT INTO user(first_name, last_name, user_email, password)
-		VALUES('$first_name', '$last_name', '$user_email', '$user_pass')";
-$result = $conn->query($sql) or die($conn->error);
+	$fail = true; 
+}
+//check if string contains any spaces in the actually trimmed string
+if(strpos($first_name, " ") === true || strpos($last_name, " ") === true || strpos($user_name, " ") === true 
+		|| strpos($user_email, " ") === true || strpos($user_pass, " ") === true || strcmp($user_pass_confirm, "") === 0) { 
 
-echo "registered";
+	$fail = true; 
+}
+//Validate email format
+if(!filter_var($user_email, FILTER_VALIDATE_EMAIL)) { $fail = true; }
+
+//Check to see if password and password confirm matches
+if(strcmp($user_pass, $user_pass_confirm) !== 0) { $fail = true; }
+
+//If no errors then try sql else echo error
+if($fail === false){
+    $sql = "INSERT INTO user(first_name, last_name, user_name, user_email, password)
+        VALUES('$first_name', '$last_name', '$user_name', '$user_email', '$user_pass')";
+    if(!mysqli_query($conn, $sql)) echo "<br>" . mysqli_error($conn); 
+} else if($fail === true){
+	echo "<br>" . "ERROR: CHECK INPUT";
+}
+
+//Close mysql connection
+mysqli_close($conn);
 ?>
 
+<br>
 <a href="../index.html"> Return to main page</a>
--- php/login.php
+++ php/login.php
@@ -2,18 +2,40 @@
 	include "db_connect.php";
 	
 	$username = $password = "";
-	
-	$username = $_POST["uname"];
-	$password = $_POST["pword"];
-	
-		
-	$sql = "select * from user where user_email='$username' and password='$password'";
-	
-	if(mysqli_num_rows(mysqli_query($conn, $sql)) > 0) {
-		echo " Success Login.";	
+	$fail = false;
+
+	if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["uname"]) && isset($_POST["pword"]))	{
+		$username = mysqli_real_escape_string($conn, strip_tags(trim($_POST["uname"])));
+		$password = mysqli_real_escape_string($conn, strip_tags(trim($_POST["pword"])));
 	} else {
-		echo " Error Login.";
+		$fail = true;
 	}
+
+	//check if empty string
+	if(strcmp($username, "") === 0 || strcmp($password, "") === 0) {
+    	$fail = true;
+	}
+	//check if string contains any spaces in the actually trimmed string
+	if(strpos($username, " ") === true || strpos($password, " ") === true) {
+    	$fail = true;
+	}
+
+
+	if($fail === false)	{
+		$sql = "select * from user where user_name='$username' and password='$password'";
+	
+		if(mysqli_num_rows(mysqli_query($conn, $sql)) == 1) {
+			echo "<br>" . "Success Login.";	
+		} else {
+			echo "<br>" . "Error Login.";
+		}
+		
+	} else {
+		echo "<br>" . "ERROR: CHECK INPUT";
+	}
+
+	mysqli_close($conn);
 ?>
 
+<br>
 <a href="../index.html"> Return to main page</a>
